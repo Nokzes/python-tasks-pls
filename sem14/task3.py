851f02bf031b5a62fe671f5309d4d656d4d34e94
Nokzes <mihalopp111@gmail.com>
2026-03-23 13:16:28 +0300

    Подправил решение

--- sem14/task3.py
+++ sem14/task3.py
@@ -6,7 +6,7 @@ def my_func(st: str):
     
     if len(st) == 4:
         if (st[0] + 1 == st[1]) and (st[1] + 1 == st[2]) and (st[2] + 1 == st[3]):
-            a = st[0] * st[1] * st[2] * st[3]
+            a = st[0] * st[1] * st[2] * st[3] + 1
             b = a**0.5
             return [a, b]
         else:
@@ -17,6 +17,5 @@ def my_func(st: str):
 print(my_func('1,2,3,4'))
 print(my_func('1,3,4,5'))
 print(my_func('123124'))
-
 
 
