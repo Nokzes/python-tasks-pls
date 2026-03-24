9c260c2fd8c7066e9d3b6eb6ebcbb222e69e798f
Nokzes <mihalopp111@gmail.com>
2026-03-24 13:07:09 +0300

    задача

---
+++ sem15/task5.py
@@ -0,0 +1,14 @@
+def my_func(arr: list) -> list:
+    res = []
+    alphabet = ' abcdefghijklmnopqrstuvwxyz'
+    vr_res = 0
+
+    for i in range(len(arr)):
+        vr_res = 0
+        for j in arr[i]:
+            vr_res += alphabet.index(j)
+        res.append(vr_res * (i+1))
+    return res
+
+print(my_func(['abc', 'a b  c', 'a', 'zxca']))
+
